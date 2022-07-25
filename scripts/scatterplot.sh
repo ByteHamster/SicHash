1e22113da8936a649ae70b56e79ee9a1952989e9
ByteHamster <info@bytehamster.com>
2022-07-25 15:48:20 +0200

    Configurable scatterplot

--- scripts/scatterplot.sh
+++ scripts/scatterplot.sh
@@ -2,10 +2,12 @@
 hostname
 strings Comparison | grep fPIC
 
-./Comparison --numKeys 5M --loadFactor 0.8
-./Comparison --numKeys 5M --loadFactor 0.85
-./Comparison --numKeys 5M --loadFactor 0.9
-./Comparison --numKeys 5M --loadFactor 0.95
-./Comparison --numKeys 5M --loadFactor 0.97
-./Comparison --numKeys 5M --recsplitOnly
-./Comparison --numKeys 1M --mphfWbpmOnly
+echo "Keys: $1"
+
+./Comparison --numKeys "$1" --recsplitOnly
+./Comparison --numKeys "$1" --loadFactor 0.8
+./Comparison --numKeys "$1" --loadFactor 0.85
+./Comparison --numKeys "$1" --loadFactor 0.9
+./Comparison --numKeys "$1" --loadFactor 0.95
+./Comparison --numKeys "$1" --loadFactor 0.97
+./Comparison --numKeys "1M" --mphfWbpmOnly

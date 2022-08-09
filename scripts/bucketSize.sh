0762bb0c3a67e10040a1d8ad428db0e5838a95bc
ByteHamster <info@bytehamster.com>
2022-08-09 16:08:53 +0200

    Extend bucket size plot

--- scripts/bucketSize.sh
+++ scripts/bucketSize.sh
@@ -2,19 +2,42 @@
 hostname
 strings SicHashBenchmark | grep fPIC
 
-for i in $(seq 25 5 85); do
-  for j in $(seq 20 5 65); do
+function runBenchmark() {
+  export averageTries
+  exec 5>&1
+  R=$(./SicHashBenchmark --numKeys 5M --bucketSize "$1" --loadFactor 0.9 --percentage2 "$2" --percentage4 "$3"|tee >(cat - >&5))
+  averageTries=$(echo "$R" | sed -n 's/\(.*\)averageTries=\([0-9.]*\) \(.*\)/\2/p')
+}
+
+for i in $(seq 40 2 85); do
+  for j in $(seq 15 2 60); do
     if [[ $((i + j)) -gt '100' ]]; then
         continue
     fi
-     ./SicHashBenchmark --numKeys 5M --bucketSize 50     --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 100    --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 500    --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 1000   --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 2000   --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 5000   --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 10000  --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 50000  --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
-     ./SicHashBenchmark --numKeys 5M --bucketSize 100000 --loadFactor 0.9 --percentage2 "$j" --percentage4 "$j"
+    echo "Trying $i $j"
+    # The larger the hash table for a single configuration, the more tries are needed.
+    # When a small bucket size already times out, a larger one with the same configuration will probably time out as well.
+    # Therefore, continue with the next loop iteration as soon as one of the methods times out.
+    timeoutTries=50
+
+    runBenchmark 100 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 200 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 500 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 1000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 2000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 5000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 10000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 20000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 50000 "$i" "$j"
+    if (( $(echo "$averageTries > $timeoutTries" | bc -l) )); then continue; fi
+    runBenchmark 100000 "$i" "$j"
   done
 done

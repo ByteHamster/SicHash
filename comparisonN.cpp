f4d05ee494a285a8cf81895ef12b509567afa715
ByteHamster <info@bytehamster.com>
2022-08-16 16:02:32 +0200

    Run 4-bit BBHash

--- comparisonN.cpp
+++ comparisonN.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
         Contender::numQueries = 5e7;
         {RecSplitContender<4>(N, 100).run();}
         {CmphContender(N, 0.95, "CHD", CMPH_CHD_PH, 0.95, 5, false).run();}
-        {BBHashContender(N, 1.75, 0).run();}
+        {BBHashContender(N, 2.3, 0).run();}
     }
     return 0;
 }

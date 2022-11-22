de0d010c952e3c8b6b61f699a0211c570aba22e3
ByteHamster <info@bytehamster.com>
2022-11-22 11:16:04 +0100

    Measure query performance

--- sicHashBenchmark.cpp
+++ sicHashBenchmark.cpp
@@ -3,9 +3,12 @@
 #include <SicHash.h>
 #include "BenchmarkData.h"
 
+#define DO_NOT_OPTIMIZE(value) asm volatile ("" : : "r,m"(value) : "memory")
+
 int main(int argc, char** argv) {
     sichash::SicHashConfig config;
     size_t N = 1e7;
+    size_t numQueries = 0;
     float t1 = 0.5;
     float t2 = 0.1;
 
@@ -15,16 +18,40 @@ int main(int argc, char** argv) {
     cmd.add_float('1', "percentage2", t1, "Threshold for objects with 2 choices");
     cmd.add_float('2', "percentage4", t2, "Threshold for objects with 4 choices");
     cmd.add_bytes('b', "bucketSize", config.smallTableSize, "Size of the small buckets (cuckoo hash tables)");
+    cmd.add_bytes('q', "numQueries", numQueries, "Number of queries");
     if (!cmd.process(argc, argv)) {
         return 1;
     }
     config.percentages(t1, t2);
     std::vector<std::string> keys = generateInputData(N);
+    std::cout << "Cooldown" << std::endl;
+    usleep(1000*1000);
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     sichash::SicHash sicHashTable(keys, config);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     long constructionTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+    long queryTime = 0;
+    if (numQueries > 0) {
+        std::cout<<"Preparing query plan"<<std::endl;
+        std::vector<std::string> queryPlan;
+        queryPlan.reserve(numQueries);
+        util::XorShift64 prng(time(nullptr));
+        for (size_t i = 0; i < numQueries; i++) {
+            queryPlan.push_back(keys[prng(N)]);
+        }
+        std::cout << "Cooldown" << std::endl;
+        usleep(1000*1000);
+        std::cout << "Querying" << std::endl;
+        begin = std::chrono::steady_clock::now();
+        for (std::string &key : queryPlan) {
+            size_t retrieved = sicHashTable(key);
+            DO_NOT_OPTIMIZE(retrieved);
+        }
+        end = std::chrono::steady_clock::now();
+        queryTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+    }
 
     std::cout << "RESULT"
               << " loadFactor=" << config.loadFactor
@@ -36,6 +63,8 @@ int main(int argc, char** argv) {
               << " bucketSize=" << config.smallTableSize
               << " averageTries=" << (double) sicHashTable.unnecessaryConstructions / sicHashTable.numSmallTables
               << " constructionTimeMillis=" << constructionTime
+              << " queryTimeMillis=" << queryTime
+              << " numQueries=" << numQueries
               << std::endl;
     return 0;
 }
